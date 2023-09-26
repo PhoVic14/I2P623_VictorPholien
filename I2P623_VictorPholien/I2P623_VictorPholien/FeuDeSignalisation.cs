14c37e29a43f65997b580733707814aa0e3ea8db
PhoVic14 <91669693+PhoVic14@users.noreply.github.com>
2023-09-26 14:21:45 +0200

    Ajout de l'interro

---
+++ I2P623_VictorPholien/I2P623_VictorPholien/FeuDeSignalisation.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace I2P623_VictorPholien
+{
+    class FeuDeSignalisation
+    {
+        private int _couleur;
+        private string _identifiant;
+        public FeuDeSignalisation(string identifiant,int couleur)
+        {
+            _identifiant = identifiant;
+            _couleur = couleur;
+
+        }
+
+        public int Couleur
+        { 
+            get { return _couleur; } 
+            set { _couleur = value; } 
+        }
+        
+        public string Identifiant
+        { 
+            get { return _identifiant; } 
+            set { _identifiant = value; } 
+        }
+
+        public void Clignote(int etat)
+        {
+            int i;
+            for (etat = 0, i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Est éteint");
+                Console.WriteLine("Est allumé");
+            }
+        }
+
+        public void Change(int etat, string identifiant)
+        {
+            int i;
+            for (etat = 0, i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Le feu de signalisation est");
+                Console.WriteLine("Le feu de signalisation est");
+                Console.WriteLine("Le feu de signalisation est");
+            }
+        }
+
+        public void AfficheEtat()
+        {
+            Console.WriteLine("Le feu de signalisation est");
+            Console.WriteLine("Le feu de signalisation est");
+        }
+    }
+}
---
+++ I2P623_VictorPholien/I2P623_VictorPholien/Program.cs
@@ -0,0 +1,22 @@
+﻿
+
+namespace I2P623_VictorPholien
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Etat des feux:");
+            Console.WriteLine("______________");
+            Console.WriteLine(AfficheEtat());
+
+            Console.WriteLine("Changement d'état");
+            Console.WriteLine("_________________");
+            Console.WriteLine(Change());
+
+            Console.WriteLine("Feu clignotant");
+            Console.WriteLine("______________");
+            Console.WriteLine(Clignote());
+        }
+    }
+}
